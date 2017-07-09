bbe8c9810893a57947d658f5a03efd2e64b8cdcf
sisay82 <Sisay82@gmail.com>
2017-07-10 00:56:01 +0200

    unsubcribe osservable on component destroy, styling bc-line

--- src/core/list/listItem/list-item.component.ts
+++ src/core/list/listItem/list-item.component.ts
@@ -2,18 +2,24 @@ import {
     Component,
     Directive,
     Input,
-    ViewEncapsulation,
+    Injectable,
     ElementRef,
     Renderer2,
-    Injectable
+    ViewEncapsulation,
+    ContentChildren,
+    QueryList,
+    OnDestroy
 } from '@angular/core';
 
+import { Subscription } from 'rxjs/Subscription';
 import { Subject } from 'rxjs/Subject';
+
+import { BcStyler } from '../../shared/types/bc-styler';
 
 let _nextListItemId: number = 0;
 
 @Injectable()
-export class ItemSelectionService {
+export class ListItemService {
     // Observable string sources
     private _SelctionsSource = new Subject<string>();
     // Observable string streams
@@ -74,22 +80,42 @@ export class BcLineStyler {
     templateUrl: 'list-item.component.html',
     encapsulation: ViewEncapsulation.None
 })
-export class BcListItem {
+export class BcListItem extends BcStyler implements OnDestroy {
     readonly listItemId: number;
+    private _linesSubcription: Subscription;
     private isNavItem: boolean = false;
+    private _linesClass: string;
+
     public listItemUniqueName: string;
     public active: boolean = false;
 
-    constructor(private _ElementRef: ElementRef, private _Renderer2: Renderer2, private _SelectionService: ItemSelectionService) {
+    constructor(private elementRef: ElementRef, _renderer2: Renderer2, private _ListItemService: ListItemService) {
+        super(elementRef, _renderer2);
+
         this.listItemId = _nextListItemId++;
     }
 
-    ngAfterViewInit() {
-        this.isNavItem = this._ElementRef.nativeElement.attributes["bc-nav-item"] !== undefined;
+    @ContentChildren(BcLineStyler) _lines: QueryList<BcListItem>;
+
+    ngAfterContentInit(): void {
+        let newClassName: string = this._lines.length > 1 ? "bc-multi-line": null;
+        this.updateClass("_linesClass", newClassName);
+        this._linesSubcription = this._lines.changes.subscribe((lines) => {
+           let newClassName: string = this._lines.length > 1 ? "bc-multi-line": null;
+            this.updateClass("_linesClass", newClassName);
+        });
+    }
+
+    ngAfterViewInit(): void {
+        this.isNavItem = this.elementRef.nativeElement.attributes["bc-nav-item"] !== undefined;
     }
 
     selected(event: any): void {
-        this._SelectionService.selectionChange(this.listItemUniqueName);
+        this._ListItemService.selectionChange(this.listItemUniqueName);
+    }
+
+    ngOnDestroy(): void {
+        this._linesSubcription.unsubscribe();
     }
 
 }
