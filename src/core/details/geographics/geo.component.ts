8c810d67a8559e5f84b24162be950be7f5bf1dc7
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2017-07-20 16:45:47 +0200

    Increased zoom ration in geographics

--- src/core/details/geographics/geo.component.ts
+++ src/core/details/geographics/geo.component.ts
@@ -40,7 +40,7 @@ export class BcGeo {
 
   getZoom(){
     if(this.data!=undefined && this.data.location!=undefined){
-      return 11;
+      return 17;
     }else{
       return 6;//default
     }
