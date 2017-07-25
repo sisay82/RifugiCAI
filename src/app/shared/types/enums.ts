ecd61c7b384928b61db27c7c6d7167b1f2e3038d
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2017-07-25 17:27:51 +0200

    Fix enums

--- src/app/shared/types/enums.ts
+++ src/app/shared/types/enums.ts
@@ -1,99 +1,105 @@
 export namespace Enums {
     //Type of section available for shelter request
     export enum ShelterSectionType {
-        Geolocation = <any>"geolocation",
-        Contacts = <any>"contacts",
-        Services = <any>"services",
-        Management = <any>"management",
-        Cadastre = <any>"cadastre"
+        "geolocation",
+        "contacts",
+        "services",
+        "management",
+        "catastal"
     }
 
     //tipo riscaldamento
     export enum Heating_Type {
-        ELETTRICO = "Elettrico",
-        GAS = "Gas",
-        SOLARE = "Solare",
-        LEGNA = "Legna",
-        ASSENTE = "Assente"
+        "Elettrico",
+        "Gas",
+        "Solare",
+        "Legna",
+        "Assente"
     }
 
     //stagioni
     export enum Seasons {
-        PRIMAVERA = "Primavera",
-        ESTATE = "Estate",
-        AUTUNNO = "Autunno",
-        INVERNO = "Inverno"
+        "Primavera",
+        "Estate",
+        "Autunno",
+        "Inverno"
     }
 
     //disponibilitá acqua
     export enum Water_Availability {
-        SCARSA = "Scarsa", 
-        COSTANTE = "Costante", 
-        MEDIA = "Media",
-        ABBONDANTE = "Abbondante"
+        "Scarsa", 
+        "Costante", 
+        "Media",
+        "Abbondante"
     }
 
     //tipologia fonte acqua
     export enum Water_Type {
-        ASSENTE = "Assente", 
-        ACQUEDOTTO = "Acquedotto", 
-        CAPTAZIONE = "Captazione"
+        "Assente", 
+        "Acquedotto", 
+        "Captazione"
     }
 
     //tipo rifugio
     export enum Shelter_Type {
-        RIFUGIO="Rifugio",
-        BIVACCO="Bivacco",
-        RIFUGIO_CUSTODITO="Rifugio custodito",
-        RIFUGIO_INCUSTODITO="Rifugio incustodito",
-        CAPANNA_SOCIALE="Capanna sociale",
-        PUNTO_dAPPOGGIO="Punto d'appoggio"
+        "Rifugio",
+        "Bivacco",
+        "Rifugio custodito",
+        "Rifugio incustodito",
+        "Capanna sociale",
+        "Punto d'appoggio"
     }
 
     //tipo rifugio regionale
     export enum Regional_Type {
-        ESCURSIONISTICO = "Escursionistico",
-        ALPINISTICO = "Alpinistico",
-        BIVACCO = "Bivacco",
-        NON_CLASSIFICABILE = "Non classificabile"
+        "Escursionistico",
+        "Alpinistico",
+        "Bivacco",
+        "Non classificabile"
     }
 
     //categoria rifugio
     export enum Shelter_Category {
-        A="A",
-        B="B",
-        C="C",
-        D="D"
+        "A",
+        "B",
+        "C",
+        "D"
     }
 
     //tipologia scarico
     export enum Drain_Type {
-        IMOF_FOGNATURA="IMOF fognatura",
-        IMOF_POZZO_PERDENTE="IMOF pozzo perdente",
-        IMOF_DISPERSORE_SOTTOSUOLO="IMOF dispersore sottosuolo"
+        "IMOF fognatura",
+        "IMOF pozzo perdente",
+        "IMOF dispersore sottosuolo"
     }
 
     //coerenza tipologica
     export enum Typo_consistency {
-        PIENA="Piena",
-        PARZIALE="Parziale",
-        RECUPERABILE="Recuperabile",
-        NESSUNA="Nessuna"
+        "Piena",
+        "Parziale",
+        "Recuperabile",
+        "Nessuna"
     }
 
     //tipo fonte risorsa
     export enum Source_Type {
-        ENERGETICA = "Energetica",
-        IDRICA = "Idrica",
-        RISCALDAMENTO = "Riscaldamento"
+        "Energetica",
+        "Idrica",
+        "Riscaldamento"
+    }
+
+    export enum Owner_Type
+    {
+        Custode,
+        Proprietario
     }
 
     //tipo custodia
     export enum Custody_Type {
-        DIRETTA = "Diretta",
-        AFFIRRO_A_GESTORE = "Affitto a gestore",
-        AFFITTO_RAMO_AZIENDA = "Affito ramo azienda",
-        CAPANNA_SOCIALE = "Capanna sociale",
-        CUSTODIA = "Custodia"
+        "Diretta",
+        "Affitto a gestore",
+        "Affito ramo azienda",
+        "Capanna sociale",
+        "Custodia"
     }
 }
