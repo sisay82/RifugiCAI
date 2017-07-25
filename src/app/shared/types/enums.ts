12ed2d239933a7e20519f454384d87e4750b12c0
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2017-07-25 15:10:04 +0200

    Changed list layout. Add water and heating in catastal

--- src/app/shared/types/enums.ts
+++ src/app/shared/types/enums.ts
@@ -8,52 +8,91 @@ export namespace Enums {
         Cadastre = <any>"cadastre"
     }
 
+    //tipo riscaldamento
+    export enum Heating_Type {
+        ELETTRICO = "Elettrico",
+        GAS = "Gas",
+        SOLARE = "Solare",
+        LEGNA = "Legna",
+        ASSENTE = "Assente"
+    }
+
+    //stagioni
+    export enum Seasons {
+        PRIMAVERA = "Primavera",
+        ESTATE = "Estate",
+        AUTUNNO = "Autunno",
+        INVERNO = "Inverno"
+    }
+
+    //disponibilitá acqua
+    export enum Water_Availability {
+        SCARSA = "Scarsa", 
+        COSTANTE = "Costante", 
+        MEDIA = "Media",
+        ABBONDANTE = "Abbondante"
+    }
+
+    //tipologia fonte acqua
+    export enum Water_Type {
+        ASSENTE = "Assente", 
+        ACQUEDOTTO = "Acquedotto", 
+        CAPTAZIONE = "Captazione"
+    }
+
     //tipo rifugio
     export enum Shelter_Type {
-        Bivacco,
-        Rifugio_Custodito,
-        Rifugio_Incustodito,
-        Capanna_Sociale,
-        Punto_Appoggio
+        BIVACCO="Bivacco",
+        RIFUGIO_CUSTODITO="Rifugio custodito",
+        RIFUGIO_INCUSTODITO="Rifugio incustodito",
+        CAPANNA_SOCIALE="Capanna sociale",
+        PUNTO_dAPPOGGIO="Punto d'appoggio"
     }
 
     //tipo rifugio regionale
     export enum Regional_Type {
-        Escursionistico,
-        Aplinistico,
-        Bivacco,
-        Non_Classificabile
+        ESCURSIONISTICO = "Escursionistico",
+        ALPINISTICO = "Alpinistico",
+        BIVACCO = "Bivacco",
+        NON_CLASSIFICABILE = "Non classificabile"
     }
 
     //categoria rifugio
     export enum Shelter_Category {
-        A,
-        B,
-        C,
-        D
+        A="A",
+        B="B",
+        C="C",
+        D="D"
+    }
+
+    //tipologia scarico
+    export enum Drain_Type {
+        IMOF_FOGNATURA="IMOF fognatura",
+        IMOF_POZZO_PERDENTE="IMOF pozzo perdente",
+        IMOF_DISPERSORE_SOTTOSUOLO="IMOF dispersore sottosuolo"
     }
 
     //coerenza tipologica
     export enum Typo_consistency {
-        Piena,
-        Parziale,
-        Recuperabile,
-        Nessuna
+        PIENA="Piena",
+        PARZIALE="Parziale",
+        RECUPERABILE="Recuperabile",
+        NESSUNA="Nessuna"
     }
 
     //tipo fonte risorsa
     export enum Source_Type {
-        Energetica,
-        Idrica,
-        Riscaldamento
+        ENERGETICA = "Energetica",
+        IDRICA = "Idrica",
+        RISCALDAMENTO = "Riscaldamento"
     }
 
     //tipo custodia
     export enum Custody_Type {
-        Diretta,
-        Affitto_a_Gestore,
-        Affito_Ramo_Azienda,
-        Capanna_Sociale,
-        Custodia
+        DIRETTA = "Diretta",
+        AFFIRRO_A_GESTORE = "Affitto a gestore",
+        AFFITTO_RAMO_AZIENDA = "Affito ramo azienda",
+        CAPANNA_SOCIALE = "Capanna sociale",
+        CUSTODIA = "Custodia"
     }
 }
