6775c4f58b3c1a1b9110e1244a9840dc72fb88c5
Lorenzo <lorenzobonicelli@libero.it>
2017-10-05 19:28:28 +0200

    Fix shelter base url

--- src/app/shelter/shelter.service.ts
+++ src/app/shelter/shelter.service.ts
@@ -14,10 +14,7 @@ import { Enums  } from '../shared/types/enums';
 @Injectable()
 export class ShelterService {
 
-    //sheltersBaseUrl: string = '/api/shelters';
-    //sheltersBaseUrl: string = 'http://31.14.133.111:27010/api/shelters';
-    //sheltersBaseUrl: string = 'http://localhost:8080/api/shelters';
-    sheltersBaseUrl: string = 'https://test-mongo-cai.herokuapp.com/api/shelters';
+    sheltersBaseUrl: string = '/api/shelters';
 
     constructor(private http: Http) { }
 
