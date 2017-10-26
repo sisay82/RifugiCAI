5387b30d7fee0ad26e0665f7f4968167e0cb8bbf
Lorenzo <lorenzobonicelli@libero.it>
2017-10-27 01:39:12 +0200

    Add tooltip with support. Fix tooltip class name

--- src/core/fixed_mask/mask-detail/mask-detail.module.ts
+++ src/core/fixed_mask/mask-detail/mask-detail.module.ts
@@ -3,11 +3,12 @@ import { BcMask } from './mask-detail.component';
 import { CommonModule } from '@angular/common';
 import { BcDividerModule } from '../../divider/divider.module';
 import {BcIconModule} from '../../icon/icon.module';
+import {BcTooltipModule} from '../../tooltip/tooltip.module';
 
 @NgModule({
     exports: [BcMask],
     declarations: [BcMask],
-    imports:[CommonModule,BcIconModule,BcDividerModule]
+    imports:[CommonModule,BcTooltipModule,BcIconModule,BcDividerModule]
 })
 export class BcMaskDetailModule{
 
